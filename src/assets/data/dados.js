bc94142ea56a2d377ab4eba6e51563174ecdc0e5
Lucas Gomes <lucas.gomes.manaus@gmail.com>
2022-10-17 21:38:07 -0400

    Bug FIX: Imagem dos produtos

--- src/assets/data/dados.js
+++ src/assets/data/dados.js
@@ -1,35 +1,42 @@
 export const Products = [
   {
     id: 1,
-    imagem: "/src/assets/image/1.png",
+    imagem:
+      "https://github.com/llucasgomes/Jordan-Shoes/blob/main/src/assets/image/1.png",
   },
   {
     id: 2,
-    imagem: "/src/assets/image/2.png",
+    imagem:
+      "https://github.com/llucasgomes/Jordan-Shoes/blob/main/src/assets/image/2.png",
   },
   {
     id: 3,
     imagem:
-      "https://akns-images.eonline.com/eol_images/Entire_Site/2016827/rs_560x822-160927104432-Poster-E-Fada-630x925.jpg?fit=around%7C560:822&output-quality=90&crop=560:822;center,top",
+      "https://github.com/llucasgomes/Jordan-Shoes/blob/main/src/assets/image/3.png",
   },
   {
     id: 4,
-    imagem: "/src/assets/image/4.png",
+    imagem:
+      "https://github.com/llucasgomes/Jordan-Shoes/blob/main/src/assets/image/4.png",
   },
   {
     id: 5,
-    imagem: "/src/assets/image/5.png",
+    imagem:
+      "https://github.com/llucasgomes/Jordan-Shoes/blob/main/src/assets/image/5.png",
   },
   {
     id: 6,
-    imagem: "/src/assets/image/6.png",
+    imagem:
+      "https://github.com/llucasgomes/Jordan-Shoes/blob/main/src/assets/image/6.png",
   },
   {
     id: 7,
-    imagem: "/src/assets/image/7.png",
+    imagem:
+      "https://github.com/llucasgomes/Jordan-Shoes/blob/main/src/assets/image/7.png",
   },
   {
     id: 8,
-    imagem: "/src/assets/image/8.png",
+    imagem:
+      "https://github.com/llucasgomes/Jordan-Shoes/blob/main/src/assets/image/8.png",
   },
 ];
