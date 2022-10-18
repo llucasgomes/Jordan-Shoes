75cc6deb8e3fef9f88dc9fb824f46749c414e221
Lucas Gomes <lucas.gomes.manaus@gmail.com>
2022-10-17 21:49:33 -0400

    resturando

--- src/assets/data/dados.js
+++ src/assets/data/dados.js
@@ -1,42 +1,34 @@
 export const Products = [
   {
     id: 1,
-    imagem:
-      "https://github.com/llucasgomes/Jordan-Shoes/blob/main/src/assets/image/tenis1.png",
+    imagem: "/src/assets/image/1.png",
   },
   {
     id: 2,
-    imagem:
-      "https://github.com/llucasgomes/Jordan-Shoes/blob/main/src/assets/image/2.png",
+    imagem: "/src/assets/image/2.png",
   },
   {
     id: 3,
-    imagem:
-      "https://github.com/llucasgomes/Jordan-Shoes/blob/main/src/assets/image/3.png",
+    imagem: "/src/assets/image/3.png",
   },
   {
     id: 4,
-    imagem:
-      "https://github.com/llucasgomes/Jordan-Shoes/blob/main/src/assets/image/4.png",
+    imagem: "/src/assets/image/4.png",
   },
   {
     id: 5,
-    imagem:
-      "https://github.com/llucasgomes/Jordan-Shoes/blob/main/src/assets/image/5.png",
+    imagem: "/src/assets/image/5.png",
   },
   {
     id: 6,
-    imagem:
-      "https://github.com/llucasgomes/Jordan-Shoes/blob/main/src/assets/image/6.png",
+    imagem: "/src/assets/image/6.png",
   },
   {
     id: 7,
-    imagem:
-      "https://github.com/llucasgomes/Jordan-Shoes/blob/main/src/assets/image/7.png",
+    imagem: "/src/assets/image/7.png",
   },
   {
     id: 8,
-    imagem:
-      "https://github.com/llucasgomes/Jordan-Shoes/blob/main/src/assets/image/8.png",
+    imagem: "/src/assets/image/8.png",
   },
 ];
