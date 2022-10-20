a7de21ed14c7c942bf8bb045c1b1cb19edaff584
Lucas Gomes <lucas.gomes.manaus@gmail.com>
2022-10-19 22:28:33 -0400

    teste2

--- src/assets/data/dados.js
+++ src/assets/data/dados.js
@@ -5,30 +5,30 @@ export const Products = [
   },
   {
     id: 2,
-    imagem: "/src/assets/image/2.png",
+    imagem: "/assets/image/2.png",
   },
   {
     id: 3,
-    imagem: "./src/assets/image/3.png",
+    imagem: "/assets/image/3.png",
   },
   {
     id: 4,
-    imagem: "./src/assets/image/4.png",
+    imagem: "/assets/image/4.png",
   },
   {
     id: 5,
-    imagem: "./src/assets/image/5.png",
+    imagem: "/assets/image/5.png",
   },
   {
     id: 6,
-    imagem: "./src/assets/image/6.png",
+    imagem: "/assets/image/6.png",
   },
   {
     id: 7,
-    imagem: "./src/assets/image/7.png",
+    imagem: "/assets/image/7.png",
   },
   {
     id: 8,
-    imagem: "./src/assets/image/8.png",
+    imagem: "/assets/image/8.png",
   },
 ];
